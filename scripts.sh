a9037a86ed9a6677ffd5c06c5aa77b19ad97c7ff
James <code-for-coffee@users.noreply.github.com>
2016-01-21 10:13:37 -0600

    Update scripts.sh

--- scripts.sh
+++ scripts.sh
@@ -1,1 +1,1 @@
-npm install --save-dev browserify babelify babel-preset-es2015 babel-preset-react vinyl-source-stream gulp-uglify
+npm install --save-dev browserify gulp babelify babel-preset-es2015 babel-preset-react vinyl-source-stream gulp-uglify gulp-watch
